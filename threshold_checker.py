fde5ad0fda5f5b84c0a44ce1dc9f3ce176ee1725
ashnaeldho <ashnaeldho12@gmail.com>
2022-12-13 19:27:28 +0530

    Create threshold_checker.py

---
+++ threshold_checker.py
@@ -0,0 +1,9 @@
+def thresholdMinExcluded(value, threshold):
+    if value < threshold:
+        return True
+    return False
+
+def thresholdMaxExcluded(value, threshold):
+    if value > threshold:
+        return True
+    return False
